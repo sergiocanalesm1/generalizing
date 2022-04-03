28cd01c60ee23d91375385181a251943e88979da
sergio <sergiocanalesm1@gmail.com>
2022-04-02 20:47:14 -0500

    fixed serializers

--- generalizing_core/api/lesson_api.py
+++ generalizing_core/api/lesson_api.py
@@ -1,10 +1,9 @@
 from django.shortcuts import get_object_or_404
 
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.decorators import api_view
 
-from generalizing_core.serializers.lesson_serializer import LessonSerializer
+from generalizing_core.serializers.lesson_serializer import LessonReadSerializer, LessonWriteSerializer
 from generalizing_core.models.lesson import Lesson
 from generalizing_core.models.user import User
 from generalizing_core.models.tag import Tag
@@ -22,18 +21,18 @@ def handle_tags(tags):
 def lesson_list(request):
     if request.data and 'tags' in request.data:
         request.data['tags'] = handle_tags(request.data['tags'])
-    return list(request,Lesson,LessonSerializer)
+    return list( request, Lesson, LessonReadSerializer, LessonWriteSerializer )
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def lesson_detail(request, uuid):
     if request.data and 'tags' in request.data:
         request.data['tags'] = handle_tags(request.data['tags'])
-    return detail(request,uuid,Lesson,LessonSerializer)
+    return detail(request,uuid,Lesson,LessonReadSerializer)
 
 @api_view(['GET'])
 def lesson_user_list(request, uuid):
     pk = get_object_or_404(User, uuid=uuid).pk
     list = Lesson.objects.filter(user_id = pk)
-    serializer = LessonSerializer(list, many=True,context={'request':request})
+    serializer = LessonReadSerializer(list, many=True,context={'request':request})
     return Response(serializer.data)
--- generalizing_core/serializers/challenge_serializer.py
+++ generalizing_core/serializers/challenge_serializer.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 
+from generalizing_core.serializers.lesson_serializer import LessonReadSerializer
 from generalizing_core.models.challenge import Challenge
 
-class ChallengeSerializer(serializers.ModelSerializer):
+class ChallengeReadSerializer(serializers.ModelSerializer):
+
+    lesson_1 = LessonReadSerializer(read_only=True)
+    lesson_2 = LessonReadSerializer(read_only=True)
+
+    class Meta:
+        model = Challenge
+        fields = '__all__'
+
+class ChallengeWriteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Challenge
