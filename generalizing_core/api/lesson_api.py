055e22df6435d48a0e7edb984d8f9a046c45bf88
sergio <sergiocanalesm1@gmail.com>
2022-05-18 10:02:08 -0500

    new challenge api

--- generalizing_core/api/lesson_api.py
+++ generalizing_core/api/lesson_api.py
@@ -28,6 +28,7 @@ def lesson_list(request):
 def lesson_detail(request, uuid):
     if request.data and 'tags' in request.data:
         request.data['tags'] = handle_tags(request.data['tags'])
+    #return detail(request,uuid,Lesson,LessonWriteSerializer, LessonReadSerializer)
     return detail(request,uuid,Lesson,LessonWriteSerializer)
 
 @api_view(['GET'])
